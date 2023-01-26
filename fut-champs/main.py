9a3115e54d5234d45cbd8a5aee0c7d07cdf6103f
mattzech <matthewjzech@gmail.com>
2023-01-25 21:13:39 -0600

    feat(main): fix return and add csv

--- fut-champs/main.py
+++ fut-champs/main.py
@@ -1,14 +1,16 @@
 import numpy as np
 
 def main():
-    getWinLossRecord(3, 9)
-
+    record = getWinLossRecord(10, 40)
+    print(record)
 def getWinLossRecord(gamesPlayed, totalPoints):
     m_list = [[4,1],[1,1]]
     A = np.array(m_list)
     inv_A = np.linalg.inv(A)
     B = np.array([totalPoints,gamesPlayed])
     X = np.linalg.inv(A).dot(B)
-    print(X)
+    wins = int(X[0])
+    losses = int(X[1])
+    return wins,losses
 if __name__ == "__main__":
     main()
